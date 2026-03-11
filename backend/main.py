f594b902415fd6955e0ddea7bd9ce85ca565d13a
Vĩnh Kỳyy <89082148+vinhky123@users.noreply.github.com>
2026-03-11 20:50:36 +0700

    add docker file

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,7 @@ app = FastAPI(title="Cloud Study Assistant API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "http://localhost", "http://localhost:80"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
